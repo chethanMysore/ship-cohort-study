ec5cfe1daa52fe485e97603ca4b2af161b653ec5
ashwinidathatri <ashwini.dathatri@gmail.com>
2019-11-17 15:06:35 +0100

    Changes made with respect to factor of HMS datatype

--- src/scripts/factor-timestamp.R
+++ src/scripts/factor-timestamp.R
@@ -5,3 +5,9 @@ factor_timestamp <- function(data_df, col_name){
     mutate_at(vars(contains(col_name)), (function(x) return(as.numeric(as.POSIXct(x)))))
   return(sample_df)
 }
+
+factor_hms <- function(data_df_1, col_name_1){
+  sample_df <- data_df_1 %>%
+    mutate_at(vars(contains(col_name_1)), (function(x) return(as.numeric(as_hms(x)))))
+  return(sample_df)
+}
--- src/scripts/test.R
+++ src/scripts/test.R
@@ -4,9 +4,11 @@ library(tidyverse)
 library(checkmate)
 library(rlist)
 library(hash)
+library(hms)
 
-source('~/Documents/DM-Project/ship-cohort-study/src/scripts/extract-features.R')
-source('~/Documents/DM-Project/ship-cohort-study/src/scripts/factor-timestamp.R')
+source('./scripts/extract-features.R')
+source('./scripts/factor-timestamp.R')
+source('./scripts/data-with-labels.R')
 
 # wave suffix
 suffix <- "(_s0|_s1|_s2)"
@@ -14,10 +16,16 @@ suffix <- "(_s0|_s1|_s2)"
 # take sample of ship_data dataset
 sample_df <- ship_dataset
 
-# extract features in all waves
-sample_df <- extract_features_with_suffix(sample_df, suffix)
+#removing data without labels
+sample_df_with_labels <- data_with_labels(sample_df)
 
+# extract features in all waves
+sample_df <- extract_features_with_suffix(sample_df_with_labels, suffix)
+
+str(sample_df)
 # factor timestamp column for exdate_ship
 col_name = "exdate_ship"
+col_name_1 = "blt_beg"
 sample_df <- factor_timestamp(sample_df, col_name)
+sample_df <- factor_hms (sample_df, col_name_1)
 
