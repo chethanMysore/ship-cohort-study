4300d8646d3c6bb7340d263fbfd9c27585c1ea5b
chethan <chethanmr.2694@gmail.com>
2019-12-16 09:55:02 +0100

    Added rulefit implementation

---
+++ src/scripts/rule-fit-implementation.R
@@ -0,0 +1,11 @@
+rule_fit <- function(data_df, class_labels){
+  rulefit_result <- NULL;
+  # set sampling method to cross validation with 5-folds
+  train.control <- trainControl(method = "cv", number = 5)
+  set.seed(42)
+  # build rule fit model
+  rulefit_result <- train(x = data_df, y = class_labels, method = caret_pre_model, trControl = train.control, ntrees = 25L)
+  rulefit_result.data <- data_df
+  importance(rulefit_result$finalModel)
+  return(rulefit_result)
+}
--- src/scripts/test.R
+++ src/scripts/test.R
@@ -16,6 +16,8 @@ library("xlsx")
 source('./scripts/extract-features.R')
 source('./scripts/factor-timestamp.R')
 source('./scripts/data-with-labels.R')
+source('./scripts/grouping-dataframes.R')
+source('./scripts/rule-fit-implementation.R')
 
 # wave suffix
 suffix <- "(_s0|_s1|_s2)"
@@ -36,37 +38,52 @@ sample_df <- extract_features_with_suffix(sample_df, suffix)
 sample_df <- factor_timestamp(sample_df, "exdate_ship")
 sample_df <- factor_hms (sample_df, "blt_beg")
 
-#Plot Missing Values for wave s0 
+# Plot Missing Values for wave s0 
 wave_s0_df <- select(sample_df, ends_with("_s0"))
 gg_miss_var(wave_s0_df, show_pct = TRUE)  #shows percentage of missing values in the column 
 gg_miss_var(wave_s0_df, show_pct = FALSE)  #shows number of missing values in the column
 vis_miss(sample_df)  #visualize missing values
 
-#Extracting evolution features
+# Extracting evolution features
 evo_features <- Evoxploit$new(sample_df, sample_df_label_column[[1]], wave_suffix = "_s")
 evo_all_features <- evo_features$all_features
 
-#extracting evolution_features for all waves
+# Extracting evolution_features for all waves
 sample_df_for_evo <- extract_features_with_suffix(evo_all_features, suffix)
 
-#Grouping by gender
+# Grouping by gender
 group_by_male <- subset(sample_df_for_evo, female_s0==0)
 group_by_female <- subset(sample_df_for_evo, female_s0==1)
 sample_df_for_evo_compare <- gender_group_compare (group_by_male, group_by_female)
 
-#appending output 
+# Appending output 
 sample_df_for_evo_withcL <- cbind(sample_df_for_evo,sample_df_label_column)
 
-#factoring liver_fat column
+# Factoring liver_fat column
 sample_df_for_evo_withcL_factored <- factor_liver_fat (sample_df_for_evo_withcL, "liver_fat")  
 
-#scaling evolution_features for all waves
+# Build Rule Fit Model
+# Import final_evo_dataset_imputed csv into evo_dataset_imputed variable 
+evo_sample_df <- evo_dataset_imputed %>%
+  mutate(liver_fat = factor(liver_fat, levels = c(1, 0), labels = c(1, 0)))
+evo_sample_df <- evo_sample_df[complete.cases(evo_sample_df),]
+sample_class_labels <- evo_sample_df$liver_fat
+sample_data_without_CL <- evo_sample_df[,1:163]
+rule_fit_model <- rule_fit(sample_data_without_CL, sample_class_labels)
+
+# validate model prediction
+model_predictions <- predict(rule_fit_model, sample_data_without_CL) 
+cmp_table <- table(factor(model_predictions, levels = levels(model_predictions)), factor(sample_class_labels, levels = levels(sample_class_labels)))
+rule_fit_model.confusion_matrix <- confusionMatrix(cmp_table)
+rule_fit_model.confusion_matrix
+
+# Scaling evolution_features for all waves
 #stand_sample_df_for_evo <- sample_df_for_evo%>%
   #mutate_at(vars(names(sample_df_for_evo)[which(sapply(sample_df_for_evo, is.numeric))])                                         
          #   ,(function(x) return((x - min(x)) / (max(x) - min(x)))))
 #str(sample_df_for_evo$smoking_s0)
 
-##Exporting to Excel
+## Exporting to Excel
 #output_file_path <- getwd()
 #output_file_name <- "sample_df_report.xlsx"
 #dataframe_to_write <- sample_df
