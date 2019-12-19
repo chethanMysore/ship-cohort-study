606dcee21b253616ddb3a38f79bf4979c594dad7
ashwinidathatri <ashwini.dathatri@gmail.com>
2019-12-19 23:28:19 +0100

    Fix: labels format

--- src/scripts/ShipCohortStudy.R
+++ src/scripts/ShipCohortStudy.R
@@ -1,6 +1,5 @@
 ShipCohortStudy <- R6::R6Class("ShipCohortStudy", private = list(
   ..wave_suffix = "(_s0|_s1|_s2)",
-  ..data_df = NULL,
   ..labels = NULL,
   ..evo_extraction_result = NULL,
   ..evo_features = NULL,
@@ -15,7 +14,7 @@ ShipCohortStudy <- R6::R6Class("ShipCohortStudy", private = list(
   ..cv_folds = NULL,
   ..model_validation_result = NULL,
   ..importance_plotname = "feature-importance.png",
-  ..vis_dir = "../visualization"
+  ..vis_dir = "/visualization"
   ),
   public = list(
     initialize = function(data_df, labels, cv_folds = 5){
@@ -25,51 +24,22 @@ ShipCohortStudy <- R6::R6Class("ShipCohortStudy", private = list(
       checkmate::assert_true(nrow(data_df) == length(labels))
       checkmate::assert_false(any(is.na(labels)))
       
-      private$..data_df <- data_df
       private$..labels <- labels
       private$..cv_folds <- cv_folds
+      private$..data_df_with_evo <- data_df
       
       if(all(purrr::map_lgl(labels, ~ checkmate::test_numeric(.x)))){  # if the labels provided is a numeric array then convert it to factor
         labels <- only_labels(labels = labels)
         labels <- labels$liver_fat
       }
-      
-      ## Extract features present in all 3 waves
-      data_df <- extract_features_with_suffix(data_df, private$..wave_suffix)
-      
-      col_names <- names(data_df)
-      ## Factor Features
-      if(length(grep("exdate_ship", col_names, value = TRUE)) > 0){
-        data_df <- factor_timestamp(data_df, "exdate_ship")
-      }
-      if(length(grep("blt_beg", col_names, value = TRUE)) > 0){
-        data_df <- factor_hms(data_df, "blt_beg")
-      }
-      
-      ## Extracting evolution features
-      private$..evo_extraction_result <- Evoxploit$new(data_df, labels, wave_suffix = "_s")
-      private$..evo_features <- private$..evo_extraction_result$evo_features
-      
-      ## Extracting evolution_features for all waves
-      ## After this step, private$..data_df has all original and evo_features available in all waves
-      private$..data_df_with_evo <- extract_features_with_suffix(private$..evo_extraction_result$all_features, private$..wave_suffix)
-      
-      ## Remove gender specific features
-      if(length(grep("female_s0", names(private$..data_df_with_evo), value = TRUE)) > 0){
-        group_by_male <- subset(private$..data_df_with_evo, female_s0==0)
-        group_by_female <- subset(private$..data_df_with_evo, female_s0==1)
-        cols_to_remove <- gender_group_compare (group_by_male, group_by_female)
-        cols_to_remove <- list.append(cols_to_remove, "female_s0")
-        private$..data_df_with_evo <- private$..data_df_with_evo[,!names(private$..data_df_with_evo) %in% cols_to_remove]
-      }
-      
+
       ## Sample data into train and validation sets
       set.seed(42)
       train <- sample(1:nrow(private$..data_df_with_evo), 2 * nrow(private$..data_df_with_evo)/3)
       test <- (-train)
       private$..train_set <- private$..data_df_with_evo[train,]
       private$..validation_set <- private$..data_df_with_evo[test,]
-      sample_class_labels <- private$..train_set$liver_fat
+      sample_class_labels <- private$..train_set$liver_fat[[1]]
       train_set <- private$..train_set[, !names(private$..train_set) %in% c("liver_fat")]
       
       ## Build Rule Fit Model
@@ -105,7 +75,7 @@ ShipCohortStudy <- R6::R6Class("ShipCohortStudy", private = list(
           cat("\tpenalty.par.value: ", private$..best_tuning_params$penalty.par.value, "\n")
           cat("\tcv_folds: ", private$..best_tuning_params$cv_folds, "\n")
         }
-        file_path = str_c(private$..vis_dir, "/", private$..importance_plotname)
+        file_path = str_c(getwd(), private$..vis_dir, "/", private$..importance_plotname)
         png(filename = file_path)
         importance(private$..rule_fit_model$finalModel)
       }
@@ -113,14 +83,6 @@ ShipCohortStudy <- R6::R6Class("ShipCohortStudy", private = list(
     }
   ),
   active = list(
-    data = function(value){
-      if(missing(value)){
-        private$..data_df
-      }
-      else{
-        stop("$data is read only", call. = FALSE)
-      }
-    },
     data_with_evo = function(value){
       if(missing(value)){
         private$..data_df_with_evo
--- src/scripts/test.R
+++ src/scripts/test.R
@@ -33,6 +33,32 @@ sample_df <- ship_dataset
 ## Removing data without labels
 sample_df <- data_with_labels(sample_df)
 
+## Extraction of labels
+sample_labels <- only_labels(data_df = sample_df)
+sample_df$liver_fat <- sample_labels
+
+## Extract features present in all 3 waves
+sample_df <- extract_features_with_suffix(sample_df, "(_s0|_s1|_s2)")
+
+## Factor Features
+sample_df <- factor_timestamp(sample_df, "exdate_ship")
+sample_df <- factor_hms(sample_df, "blt_beg")
+
+## Extracting evolution features
+evo_extraction_result <- Evoxploit$new(sample_df, sample_labels[[1]], wave_suffix = "_s")
+sample_df <- evo_extraction_result$all_features
+
+## Extracting evolution_features for all waves
+## After this step, private$..data_df has all original and evo_features available in all waves
+sample_df <- extract_features_with_suffix(sample_df, "(_s0|_s1|_s2)")
+
+## Remove gender specific features
+group_by_male <- subset(sample_df, female_s0==0)
+group_by_female <- subset(sample_df, female_s0==1)
+cols_to_remove <- gender_group_compare (group_by_male, group_by_female)
+cols_to_remove <- list.append(cols_to_remove, "female_s0")
+sample_df <- sample_df[,!names(sample_df) %in% cols_to_remove]
+
 ## Impute Data
 sample_df <- impute_dataset(sample_df)
 
@@ -41,22 +67,22 @@ sample_df <- sample_df%>%
   mutate_at(vars(names(sample_df)[which(sapply(sample_df, is.numeric))])
             ,(function(x) return((x - min(x)) / (max(x) - min(x)))))
 
-## Extraction of labels
-sample_df_label_column <- only_labels(data_df = sample_df)
+sample_df$liver_fat <- sample_labels
 
-# ## Plot Missing Values for wave s0
+## Plot Missing Values for wave s0
 wave_s0_df <- select(sample_df, ends_with("_s0"))
 gg_miss_var(wave_s0_df, show_pct = TRUE)  #shows percentage of missing values in the column
 gg_miss_var(wave_s0_df, show_pct = FALSE)  #shows number of missing values in the column
 vis_miss(sample_df)  #visualize missing values
 
-# ## Build Rule Fit Model
-ship_study_results <- ShipCohortStudy$new(data_df = sample_df, labels = sample_df_label_column[[1]])
+## Build Rule Fit Model
+ship_study_results <- ShipCohortStudy$new(data_df = sample_df, labels = sample_df$liver_fat[[1]])
+
 ship_study_results$summary()
 
-# ## Validate Model Prediction
+## Validate Model Prediction
 validation_set <- ship_study_results$validation_set
-actual_labels <- validation_set$liver_fat
+actual_labels <- validation_set$liver_fat[[1]]
 validation_set <- validation_set[, !names(validation_set) %in% c("liver_fat")]
 model_predictions <- predict(ship_study_results$model, validation_set)
 cmp_table <- table(factor(model_predictions, levels = levels(model_predictions)),
