ff839efa51c45245be84a287250d9c9b872c5ffc
Ashwini Dathatri <ashwini.dathatri@gmail.com>
2020-03-04 22:18:00 +0100

    Feature importance  description updated

--- src/scripts/api-functions.R
+++ src/scripts/api-functions.R
@@ -21,13 +21,26 @@ test.data <- list(
   2,
   "butterfly"
 )
+# 
+# #* @get /getFeatureImportance
+# #* @serializer unboxedJSON
+# getFeatureImportance <- function() {
+#   imp <- print(ship_study_results$model$finalModel)
+#   x <- imp$description
+#   y <- imp$coefficient
+#   importance_data <- list(x,y)
+#   list('response' = importance_data)
+# }
+
 
 #* @get /getFeatureImportance
 #* @serializer unboxedJSON
 getFeatureImportance <- function() {
-  imp <- print(ship_study_results$model$finalModel)
-  x <- imp$description
-  y <- imp$coefficient
-  importance_data <- list(x,y)
+  feature_imp <- print(importance(ship_study_results$model$finalModel))
+  x <- feature_imp$varimps$imp
+  y <- feature_imp$varimps$varname
+  y_description <- list("Somatometric measurements of waist", "Time of lights off during sleep", "Glucose", "Evolution feature", "Triglycerides during S2 wave", "Triglycerides during S2 wave", "Somatometric measurements of Body Mass Index", "Triglycerides during S1 wave", "Diabetes", "Serum Uric Acid", "Evolution feature")
+  importance_data <- list(x,y,y_description)
   list('response' = importance_data)
 }
+
