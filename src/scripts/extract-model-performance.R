307dda101c6ae03b895da5232d2c9958a2f7487a
chethan <chethanmr.2694@gmail.com>
2020-03-15 14:00:35 +0100

    missed file checkin

---
+++ src/scripts/extract-model-performance.R
@@ -0,0 +1,18 @@
+extract_model_performance <- function(model, train_set, validation_set){
+  ## Validate Model Prediction
+  actual_labels <- validation_set$liver_fat
+  validation_set <- validation_set[, !names(validation_set) %in% c("liver_fat")]
+  model_predictions <- predict(model, validation_set)
+  cmp_table <- table(factor(model_predictions, levels = levels(model_predictions)),
+                     factor(actual_labels, levels = levels(actual_labels)))
+  test_performance <- confusionMatrix(cmp_table)
+  ## Model Prediction on training set
+  actual_labels <- train_set$liver_fat
+  train_set <- train_set[, !names(train_set) %in% c("liver_fat")]
+  model_predictions <- predict(model, train_set)
+  cmp_table <- table(factor(model_predictions, levels = levels(model_predictions)),
+                     factor(actual_labels, levels = levels(actual_labels)))
+  train_performance <- confusionMatrix(cmp_table)
+  
+  return(list(train_performance=train_performance, test_performance=test_performance))
+}
