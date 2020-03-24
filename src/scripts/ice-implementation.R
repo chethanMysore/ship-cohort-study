395c63543f2ae9403e5e9b8f25be6600869c0cfc
chethan <chethanmr.2694@gmail.com>
2020-03-24 17:02:02 +0100

    small fix: added feature name in respons eof geticeCoords

--- src/scripts/ice-implementation.R
+++ src/scripts/ice-implementation.R
@@ -32,6 +32,6 @@ get_ice_points <- function(data, model, feature_name, frac_to_build = 0.2){
     apply(ship_ice_curves, 1, get_individual_ice_points, as.numeric(as.vector(colnames(ship_ice_curves))))
     pdp_points = data.frame("x"=as.numeric(rownames(pdp_points)), "y"=pdp_points$`ship_ice_plot$pdp`)
   }
-  return(list(ice_points = individual_ice_points, pdp_points = pdp_points))
+  return(list(icePoints = individual_ice_points, pdpPoints = pdp_points, featureName=feature_name))
 }
 
